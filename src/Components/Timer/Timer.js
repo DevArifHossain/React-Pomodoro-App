29f539a511d5ac28ea0cb6958927debe2f2f45e2
devarifhossain <arif.dev.jibon@gmail.com>
2019-04-11 14:22:52 +0600

    okay, somehow managed to NOT getting netative times with few conditions

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -1,51 +1,63 @@
-import React, {Component} from 'react'
-import './timer.css'
+import React, { Component } from "react";
+import "./timer.css";
 
 class Timer extends Component {
-    state = {
-        min: 25,
-        sec: 0 + '0'
-    }
-    onTimerStart = () => {
-        const calcTime = () => {
-            const min = this.state.min
-            const sec = this.state.sec
+  state = {
+    min: 25,
+    sec: 0 + "0"
+  };
 
-            this.setState({
-                sec: (sec - 1).toString().padStart(2, '0') // -> adding '0' at first when num is less than 10
-            })
+  onTimerStart = () => {
+    const calcTime = () => {
+      const min = this.state.min;
+      const sec = this.state.sec;
 
-            if(sec <= 0) {
-                this.setState({
-                    sec: 59,
-                    min: (min -1).toString().padStart(2, '0')
-                })
-            }
-        }
-
-            // this -> makes stuff global so other functions can use this (not sure if i'm right with this statement but i hope so)
-        this.increment = setInterval(calcTime, 1000)
-    }
-
-    onTimerReset = () => {
-        clearInterval(this.increment)
+      if (sec > 0) {
         this.setState({
-            min: 25, 
-            sec: 0 + '0'
-        })
-    }
+          sec: (sec - 1).toString().padStart(2, "0") // -> adding '0' at first when num is less than 10
+        });
+      } else {
+        if (sec == 0 && min == 0) {
+          // trying to NOT get any negative time
+          clearInterval(this.increment);
+        } else if (sec <= 0) {
+          this.setState({
+            sec: 59,
+            min: (min - 1).toString().padStart(2, "0")
+          });
+        }
+      }
+    };
 
-    render() {
-        return (
-            <div id="timer-con">
-                <div class="timer">{this.state.min}:{this.state.sec}</div>
-                <div className="btns">
-                    <button className="btn start" onClick={this.onTimerStart}>Start</button>
-                    <button className="btn reset" onClick={this.onTimerReset}>Reset</button>
-                </div>
-            </div>
-        )
-    }
+    // this -> makes stuff global so other functions can use this (not sure if i'm right with this statement but i hope so)
+    this.increment = setInterval(calcTime, 1000);
+  };
+
+  onTimerReset = () => {
+    clearInterval(this.increment);
+    this.setState({
+      min: 25,
+      sec: 0 + "0"
+    });
+  };
+
+  render() {
+    return (
+      <div id="timer-con">
+        <div class="timer">
+          {this.state.min}:{this.state.sec}
+        </div>
+        <div className="btns">
+          <button className="btn start" onClick={this.onTimerStart} disabled={this.state.min !== 25}>
+            Start
+          </button>
+          <button className="btn reset" onClick={this.onTimerReset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Timer
+export default Timer;
